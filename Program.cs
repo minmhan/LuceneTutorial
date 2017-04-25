f879b244483cf37a15450d9e66880165e2cc9c32
Min Min Han <Min Min Han>
2017-04-25 17:23:22 +0800

    initialize

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net;
+using Lucene.Net.Analysis;
+
+namespace LuceneTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+}
